ecf8aa070acee4b87eb7d192f2f0a50227096bd0
mrodavis <orville.davis@scrapit.io>
2025-08-14 13:04:24 -0400

    added PetDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import * as petService from './services/petService.js';
 
-import PetList from './components/PetList/PetList'
+import PetList from './components/PetList/PetList';
+import PetDetail from './components/PetDetail/PetDetail';
 
 const App = () => {
   const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+
 
   // Create a new useEffect
   useEffect(() => {
@@ -24,9 +28,14 @@ const App = () => {
     fetchPets();
   }, []);
 
+  const handleSelect = (pet) => {
+    setSelected(pet);
+  };
+
   return (
     <>
-      <PetList pets={pets} />
+      <PetList pets={pets} handleSelect={handleSelect}/>
+      <PetDetail selected={selected} />
     </>
   );
 };
--- src/components/PetList/PetList.jsx
+++ src/components/PetList/PetList.jsx
@@ -11,7 +11,13 @@ const PetList = (props) => {
         ) : (
           <ul>
             {props.pets.map((pet) => (
-              <li key={pet._id}>{pet.name}</li>
+            <li 
+              key={pet._id}
+              style={{ cursor: 'pointer', color: "#646CFF" }}
+              onClick={() => props.handleSelect(pet)}
+            >
+              {pet.name}
+            </li>
             ))}
           </ul>
         )}
