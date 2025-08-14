7f95ebfc43000bc56ec5b56935e08e90e9a8d07e
mrodavis <orville.davis@scrapit.io>
2025-08-14 12:19:04 -0400

    complete READ index functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-// src/App.jsx
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService.js';
+
+import PetList from './components/PetList/PetList'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+
+  // Create a new useEffect
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index();
+        // Don't forget to pass the error object to the new Error
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        }
+        setPets(fetchedPets);
+      } catch (err) {
+        // Log the error object
+        console.log(err);
+      }
+    };
+    fetchPets();
+  }, []);
+
+  return (
+    <>
+      <PetList pets={pets} />
+    </>
+  );
 };
+
 
 export default App;
---
+++ src/services/petService.js
@@ -0,0 +1,17 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`
+// localhost:3000/pets
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL);
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+
+export {
+    index,
+}
